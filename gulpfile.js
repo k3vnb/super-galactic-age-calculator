5456e497edc02e6335b06713fd1501d561813b57
Kevin Boyle <papershack@gmail.com>
2018-01-05 12:09:42 -0800

    add npm del to build 'clean' gulp task, make clean a dependency of build gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,11 @@ gulp.task("minifyScripts", ["jsBrowserify"], function(){
     .pipe(gulp.dest("./build/js"));
 });
 
-gulp.task("build", function(){
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function(){
   if (buildProduction) {
     gulp.start('minifyScripts');
   } else {
