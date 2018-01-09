716ba91644c15bcd913ec272a7c84b49db5963cd
Kevin Boyle <papershack@gmail.com>
2018-01-08 22:56:45 -0800

    get third spec test to work

--- spec/calc-spec.js
+++ spec/calc-spec.js
@@ -16,14 +16,25 @@ describe('Person', function() {
   });
 
 });
+
 describe('AgeCompare', function() {
-  it('calculates the difference between a birthdate and now in seconds, days, and weeks', function() {
+  it('calculates the difference between a user input birthdate and a later date representing now in seconds, days, and weeks', function() {
     const birthday = '12-02-1990';
     const nowTest = '01-31-2018';
     const user1 = new AgeCompare(nowTest, birthday)
     expect(user1.secondsExactly()).toEqual(857260800);
     expect(user1.daysExactly()).toEqual(9922);
     expect(user1.weeksExactly()).toEqual(1417);
+
+  });
+  it('calculates the difference between a user input birthdate and a later date representing now in seconds, days, and weeks', function() {
+    const birthday = '12-02-1990';
+    const nowTest = '01-31-2018';
+    const user1 = new AgeCompare(nowTest, birthday)
+    expect(user1.mercuryYears()).toEqual(113.1873146246863);
+    expect(user1.venusYears()).toEqual(43.8144443708463);
+    expect(user1.marsYears()).toEqual(14.44944442017272);
+    expect(user1.jupiterYears()).toEqual(2.290468424108323);
 
   });
 
